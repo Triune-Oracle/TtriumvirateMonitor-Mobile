1db97b3f8d11528eb403ca35866ab3cd542db88c
Triune-Oracle <schlagetorren@gmail.com>
2025-05-13 05:16:11 -0700

    Create package.js
    initial commit

---
+++ package.js
@@ -0,0 +1,18 @@
+{
+  "name": "TriumvirateMonitorMobile",
+  "version": "1.0.0",
+  "main": "node_modules/expo/AppEntry.js",
+  "scripts": {
+    "start": "expo start"
+  },
+  "dependencies": {
+    "react": "18.2.0",
+    "react-native": "0.73.4",
+    "react-native-svg": "13.9.0",
+    "react-native-svg-charts": "^5.4.0"
+  },
+  "devDependencies": {
+    "babel-preset-expo": "^9.2.0"
+  },
+  "private": true
+}
